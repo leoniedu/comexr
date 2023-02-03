51a06c836336c14b9fe0ba9729c4c9140950fa3e
leoniedu <e.leoni@gmail.com>
2023-02-03 17:13:46 +0000

    comment memoise

--- R/zzz.R
+++ R/zzz.R
@@ -1,5 +1,7 @@
 # zzz.R
 .onLoad <- function(libname, pkgname) {
+  ## memoise saves computing time by storing results
+  ## memoise from scratch while loading package
   ncms <<- memoise::memoise(ncms)
   ym <<- memoise::memoise(ym)
   comexstat <<- memoise::memoise(comexstat)
